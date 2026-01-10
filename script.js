9c6f5e251ed8a0658ede9951ba62b5e9ce741438
beenehdev <beenehdev@gmail.com>
2026-01-09 21:24:03 -0800

    add dynamic div and subdiv creation with loop from user input

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const container = document.getElementById("container");
+const userRoot = 16;
+let rootDiv = null; 
 
-for (let i = 0; i < 256; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("newDiv")
-    newDiv.dataset.index = i;
-    newDiv.addEventListener("mouseover", drawOnDiv());
-    container.appendChild(newDiv);
+for (let i = 0; i < userRoot*userRoot; i++) {
+
+    if (i % userRoot === 0) {
+        const rootDiv = document.createElement("div");
+        rootDiv.classList.add("genericDiv", "rootDiv",);
+        rootDiv.dataset.index = i;
+        rootDiv.addEventListener("mouseover", drawOnDiv);
+
+        container.appendChild(rootDiv);
+        continue; 
+    };
+
+    const branchDiv = document.createElement("div");
+    branchDiv.classList.add("genericDiv", "branchDiv",)
+    branchDiv.dataset.index = i;
+    branchDiv.addEventListener("mouseover", drawOnDiv);
+
+    rootDiv.appendChild(branchDiv);
 };
 
 function drawOnDiv() {}; 
