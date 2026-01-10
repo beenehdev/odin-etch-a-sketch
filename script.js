cc25cd51f4a654f0464f4eede9580d16b12d3f40
beenehdev <beenehdev@gmail.com>
2026-01-09 20:17:13 -0800

    add mouseover eventListener to newDiv with empty fnc

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const container = document.getElementById("container");
 
 for (let i = 0; i < 256; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("newDiv")
     newDiv.dataset.index = i;
-}
+    newDiv.addEventListener("mouseover", drawOnDiv());
+    container.appendChild(newDiv);
+};
+
+function drawOnDiv() {}; 
